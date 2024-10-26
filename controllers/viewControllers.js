e933356702c8463682153fb6b015879b04904c94
martucazpo <martucazpo@gmail.com>
2024-10-26 05:39:20 -0700

    pug login and registration form mixin

--- controllers/viewControllers.js
+++ controllers/viewControllers.js
@@ -3,8 +3,24 @@
 
 const viewControllers = {
     showLanding: (req,res) => res.render("landing"),
-    showLogin: (req,res) => res.render("landing",{path: "/login"}),
-    showRegistration: (req,res) => res.render("landing", {path: "/register"}),
+    showLogin: (req,res) => {
+        let data = {
+            path: "/login",
+            action: "/auth/login",
+            btnTxt: "Login",
+            inputs: [{name: "email"}, {name:"password"}]
+        }
+        return res.render("landing",data)
+    },
+    showRegistration: (req,res) => { 
+        let data = {
+            path: "/register",
+            action: "/auth/register",
+            btnTxt: "Register",
+            inputs: [{name: "firstName"}, {name: "lastName"}, {name: "email"}, {name: "password"}, {name: "password2"}]
+        }
+        return res.render("landing", data)
+    },
     showAppLanding: (req, res) => res.render(""),
 }
 
